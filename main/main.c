89a9fe453724c6626ab5548c5cf2fa99adc74483
Alex <awkinley@gmail.com>
2019-08-31 11:25:38 -0400

    init commit

--- main/main.c
+++ main/main.c
@@ -29,8 +29,8 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_WIFI_SSID "gigawatt"
-#define EXAMPLE_WIFI_PASS "10bowk26"
+#define EXAMPLE_WIFI_SSID ""
+#define EXAMPLE_WIFI_PASS ""
 
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t wifi_event_group;
